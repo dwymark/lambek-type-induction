b9467c13aceab1c5d7eb40dd0520b9385ef0701d
Daniel Wymark <dwymark@ucla.edu>
2020-06-20 15:39:15 -0700

    Improve lambek type structure, add logging config

--- lambek.py
+++ lambek.py
@@ -1,21 +1,47 @@
 import logging
+import time
+from typing import List
+
 from lark import Lark, Transformer, v_args
 
-logging.basicConfig(level=logging.DEBUG)
+# Logging config
+# ----------------------------------------------------------------------
+DEBUGGING = True
+UNIQUE_FILE = False
 
+_filename = "debug.log"
+if UNIQUE_FILE:
+    _filename = f"{str(int(time.time() * 10000))}_" + _filename
+
+if DEBUGGING:
+    logging.basicConfig(
+        filename=_filename, level=logging.DEBUG,
+    )
+else:
+    logging.basicConfig(
+        filename=_filename, level=logging.WARNING,
+    )
+
+# Parsing
+# ----------------------------------------------------------------------
 _type_grammar = r"""
     start: type -> pass_through
 
-    type: PRIMITIVE -> build_primitive
-        | compound  -> pass_through
+    type: PRIMITIVE    -> build_primitive
+        | compound     -> pass_through
+        | product      -> pass_through
+        | "(" type ")" -> pass_through
 
-    compound: "(" compound_inner ")" -> pass_through
-            | compound_inner         -> pass_through
+    compound: compound_inner -> pass_through
 
-    compound_inner: type OVER type            -> build_compound
-                  | type UNDER type           -> build_compound
-                  | type TIMES type           -> build_compound
+    compound_inner: type OVER type            -> build_right_residue
+                  | type UNDER type           -> build_left_residue
                   | type UNDER type OVER type -> build_vee
+
+    product: product       TIMES type -> expand_product
+           | product_inner TIMES type -> expand_product
+
+    product_inner: type -> build_product
 
     OVER: "/"
     UNDER: /\\/
@@ -27,22 +53,29 @@ _type_grammar = r"""
 
 
 @v_args(inline=True)
-class LambekTypeBuilder(Transformer):
+class TypeBuilder(Transformer):
     def build_primitive(self, name: str):
         return Primitive(name)
 
-    def build_compound(self, type_a, operator, type_b):
-        return Compound(operator, type_a, type_b)
+    def build_left_residue(self, type_a, _, type_b):
+        return LeftResidue(type_a, type_b)
 
-    def build_vee(self, type_a, under, type_b, over, type_c):
-        return Compound(over, Compound(under, type_a, type_b), type_c)
+    def build_right_residue(self, type_a, _, type_b):
+        return RightResidue(type_a, type_b)
+
+    def expand_product(self, product, _, type_):
+        product.operands.append(type_)
+        return product
+
+    def build_product(self, type_):
+        return Product([type_])
 
     def pass_through(self, rhs):
         return rhs
 
 
 _parser = Lark(_type_grammar)
-_transformer = LambekTypeBuilder()
+_transformer = TypeBuilder()
 
 
 def _parse(string):  # DEBUG
@@ -51,7 +84,7 @@ def _parse(string):  # DEBUG
 
 def try_parse(string):
     """Try to parse string as a lambek type.
-    If the parse succeeds, return a LambekType.
+    If the parse succeeds, return a Type.
     If the parse fails, return None.
     """
     try:
@@ -59,14 +92,22 @@ def try_parse(string):
     except Exception:  # pylint: disable=broad-except
         logging.error("Lambek: Parsing failed for %s", string, exc_info=True)
         return None
-    return _transformer.transform(tree)
+
+    try:
+        return _transformer.transform(tree)
+    except Exception:  # pylint: disable=broad-except
+        logging.error("Lambek: Transform failed for %s", string, exc_info=True)
+
+    return None
 
 
-class LambekType:
+# Lambek Types
+# ----------------------------------------------------------------------
+class Type:
     pass
 
 
-class Primitive(LambekType):
+class Primitive(Type):
     def __init__(self, name):
         self.name = name
 
@@ -77,19 +118,48 @@ class Primitive(LambekType):
         return self.name
 
 
-class Compound(LambekType):
-    def __init__(self, operator, leftOperand, rightOperand):
-        self.operator = operator
-        self.leftOperand = leftOperand
-        self.rightOperand = rightOperand
+class Residue(Type):
+    def __init__(self, lhs: Type, rhs: Type):
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def __repr__(self):
+        return str(self)
+
+
+class LeftResidue(Residue):
+    def __str__(self):
+        return f"({self.lhs}\\{self.rhs})"
+
+
+class RightResidue(Residue):
+    def __str__(self):
+        return f"({self.lhs}/{self.rhs})"
+
+
+class Product(Type):
+    def __init__(self, operands: List[Type]):
+        self.operands = operands
 
     def __repr__(self):
         return str(self)
 
     def __str__(self):
-        return f"({self.leftOperand}{self.operator}{self.rightOperand})"
+        string = ""
+        for operand in self.operands[:-1]:
+            string += str(operand) + "*"
+        string += str(self.operands[-1])
+        return string
 
 
+class Sequent:
+    def __init__(self, lhs: List[Type], rhs: Type):
+        pass
+
+# Main
+# ----------------------------------------------------------------------
 if __name__ == "__main__":
     a = try_parse(r"(a/b)\c")
     b = try_parse(r"(a\b/c)/(a\b)")
+    c = try_parse(r"(a/b)*b*c")
+    d = try_parse(r"(a/b)*b*(c)")
